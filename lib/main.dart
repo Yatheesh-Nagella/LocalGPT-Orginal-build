0fdafd0868da5d8832a7aaadd7624c9a0ec29074
Yatheesh Nagella <yatheeshnagella17@gmail.com>
2024-09-17 19:44:09 -0500

    L6 12 test and fix bugs in using Firebase plugins

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const FirebaseTemplateApp());
 }
 
-class FirebaseTemplateApp extends StatelessWidget{
+class FirebaseTemplateApp extends StatelessWidget {
   const FirebaseTemplateApp({super.key});
 
   @override
-  Widget build(Object context){
-    return MaterialApp();
+  Widget build(Object context) {
+    return MaterialApp(
+      home: const Text('home'),
+    );
   }
 }
