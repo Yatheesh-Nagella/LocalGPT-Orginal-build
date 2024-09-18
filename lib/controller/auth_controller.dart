3853798b724bf869be5b896b8561413099a5564d
Yatheesh Nagella <yatheeshnagella17@gmail.com>
2024-09-17 20:07:32 -0500

    L6 14 auth controller signin signout

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<User?> firebaseSignIn({required String email, required String password}) async{
+  UserCredential credential = await
+  FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+  return credential.user;
+}
+
+Future<void> firebaseSignOut() async{
+  await FirebaseAuth.instance.signOut();
+}
