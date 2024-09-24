22393ea16864ee6f02e9d1b8a606d74ec28b771d
Yatheesh Nagella <yatheeshnagella17@gmail.com>
2024-09-23 19:12:04 -0500

    L6 22 sign out

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,10 @@
+import 'package:lesson6/view/home_screen.dart';
+
+class HomeController {
+  HomeState state;
+  HomeController(this.state);
+
+  Future<void> signOut() async{
+
+  }
+}
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:lesson6/controller/home_controller.dart';
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({super.key});
@@ -10,6 +10,15 @@ class HomeScreen extends StatefulWidget{
 }
 
 class HomeState extends State<HomeScreen>{
+
+  late HomeController con;
+
+  @override
+  void initState(){
+    super.initState();
+    con = HomeController(this);
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -17,6 +26,25 @@ class HomeState extends State<HomeScreen>{
         title: const Text("Home"),
       ),
       body: const Text('Home'),
+      drawer: drawerView(context),
+    );
+  }
+
+  Widget drawerView(BuildContext context){
+    return Drawer(
+      child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: const Text('No profile'),
+            accountEmail: const Text('email'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Sign out'),
+            onTap: con.signOut,
+          )
+        ],
+      ),
     );
   }
 }
