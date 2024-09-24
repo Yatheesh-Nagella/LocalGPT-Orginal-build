ec65391df4ae88e52ac0b4486eb3760b22b344bc
Yatheesh Nagella <yatheeshnagella17@gmail.com>
2024-09-24 16:33:38 -0500

    L6 32 create account in firebase auth

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -2,12 +2,37 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 User? currentUser;
 
-Future<User?> firebaseSignIn({required String email, required String password}) async{
-  UserCredential credential = await
-  FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+Future<User?> firebaseSignIn(
+    {required String email, required String password}) async {
+  UserCredential credential = await FirebaseAuth.instance
+      .signInWithEmailAndPassword(email: email, password: password);
   return credential.user;
 }
 
-Future<void> firebaseSignOut() async{
+Future<void> firebaseSignOut() async {
   await FirebaseAuth.instance.signOut();
+}
+
+/*
+Future<void> firebaseCreateAccount({
+  required String email,
+  required String password,
+}) async {
+  await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    email: email,
+    password: password,
+  );
+}
+*/
+
+firebaseCreateAccount({
+  required String email,
+  required String password,
+}) async {
+  UserCredential credential =
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    email: email,
+    password: password,
+  );
+  return credential.user;
 }
