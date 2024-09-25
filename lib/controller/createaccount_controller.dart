2e8748c7c17ba24711c7be1846dbc1f5f0e5d776
Yatheesh Nagella <yatheeshnagella17@gmail.com>
2024-09-24 20:38:30 -0500

    L6 34 create a new account

--- lib/controller/createaccount_controller.dart
+++ lib/controller/createaccount_controller.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: avoid_print, use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
+import 'package:lesson6/controller/auth_controller.dart';
 import 'package:lesson6/view/createaccount_screen.dart';
 import 'package:lesson6/view/show_snackbar.dart';
 
@@ -15,7 +19,7 @@ class CreateAccountController {
     }
   }
 
-  void createAccount() {
+  Future<void> createAccount() async {
     FormState? currentState = state.formKey.currentState;
     if (currentState == null) return;
     if (!currentState.validate()) return;
@@ -28,6 +32,41 @@ class CreateAccountController {
         seconds: 20,
       );
       return;
+    }
+
+    state.callSetState(() => state.model.inProgress = true);
+
+    try {
+      await firebaseCreateAccount(
+        email: state.model.email!,
+        password: state.model.password!,
+      );
+      state.model.email = '';
+      state.model.password = '';
+      state.model.passwordConfirm = '';
+      if (state.mounted) {
+        showSnackbar(
+          context: state.context,
+          message: 'Account created. Press BACK to go home',
+          seconds: 20,
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      print('=======> create account error: ${e.code} ${e.message}');
+      showSnackbar(
+        context: state.context,
+        message: '${e.code} ${e.message}',
+        seconds: 20,
+      );
+    } catch (e) {
+      print('=======> create account error: $e');
+      showSnackbar(
+        context: state.context,
+        message: 'Create account error: $e',
+        seconds: 20,
+      );
+    } finally{
+      state.callSetState(() => state.model.inProgress = false);
     }
   }
 }
